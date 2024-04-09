f531783d77e9de685f8a590bf48ec2ed857eaadb
Ankur kumar <ankur.malik.7967@gmail.com>
2024-04-09 15:36:29 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+const publicPath = path.join(__dirname, 'public');
+
+app.get('/', (req, resp) => {
+  resp.sendFile(`${publicPath}/index.html`); 
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/about.html`); 
+  });
+
+app.get('/projects', (req, res) => {
+    res.sendFile(`${publicPath}/projects.html`); 
+  }); 
+app.get('/contact', (req, res) => {
+    res.sendFile(`${publicPath}/contact.html`); 
+  });   
+
+app.get('*', (req, res) => {
+    res.sendFile(`${publicPath}/help.html`); 
+  });
+ 
+app.listen(5000);
